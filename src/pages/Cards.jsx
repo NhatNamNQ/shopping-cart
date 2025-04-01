b616dccaf113ffb1c375d85e8f4174a49e431cc5
NhatNamNQ <138838322+NhatNamNQ@users.noreply.github.com>
2025-04-01 04:05:54 +0000

    Create Loader component

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { getCards } from "../../api"
+import Loader from "../../components/Loader";
 
 export default function Cards() {
     const [cards, setCards] = useState([]);
@@ -24,10 +25,13 @@ export default function Cards() {
     return (
         <>
             {loading && (
-                <div>Loading...</div>
+                <Loader />
             )}
 
             <div className="container mx-auto py-8 px-6">
+                <div className="flex justify-center">
+                    <h1 className="text-4xl pb-2 font-bold mx-auto mb-10 bg-gradient-to-r from-blue-600 via-purple-600 to-blue-600 inline-block text-transparent bg-clip-text">Trading Card Game</h1>
+                </div>
                 <ul className="grid grid-cols-1 lg:grid-cols-5 md:grid-cols-3 gap-8">
                     {cards &&
                         cards.map(card => (
